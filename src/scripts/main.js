6a39343b5936a35ecfd6f1ae96194027427f32c6
Corri Golden <corri.golden@gmail.com>
2020-06-17 11:26:31 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+console.log("yo")
+
+// $(document).ready(function () {
+//     $('.submit').click(function (event) {
+//         console.log('Clicked Button')
+
+//         // var name = $('.name').val()
+//         var email = $('.email').val()
+//         // var phone = $('.phone').val()
+//         // var message = $('.message').val()
+//         var statusElm = $('.status')
+//         statusElm.empty()
+
+//         if(email.length > 5 && email.includes('@') && email.includes('.')) {
+//             statusElm.append('<div>Email is valid</div>')
+//         } else {
+//             event.preventDefault()
+//             statusElm.append('<div>Email is not valid</div>')
+//         }
+
+        
+//     })
+// })
+
+
